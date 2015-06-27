df31599c44d641cf73379dde895c65a297669b05
Mark Heimann <markheimann@Marks-MacBook-Pro.local>
2015-06-26 17:11:28 -0700

    first approximation at rwmd, though need to debug

---
+++ approx_wmd.py
@@ -0,0 +1,123 @@
+#start
+import pdb, sys, numpy as np, pickle, multiprocessing as mp
+from scipy.stats import mode
+#TODO: weight all words by their probability mass (frequency count)--at this point figure out how to get true nbow representation
+#TODO: implement WCD, optionally full WMD
+#TODO: debug so that results are OK
+def distance(x1,x2):
+    return np.sqrt( np.sum((np.array(x1) - np.array(x2))**2) )
+
+#Get word centroid of each document (represent each document as a weighted average of its word vectors)
+def getWordCentroid(document):
+	pass
+
+#Compute the Word Centroid Distance
+def wcd(doc1, doc2):
+	return distance(getWordCentroid(doc1), getWordCentroid(doc2))
+
+#Given a word and another document, find the word's nearest neighbor word vector in the other document
+def find_wordNN(word, document):
+	#Keep track of nearest word (by its index in the document) and its distance to the word in question
+	nearest_neighbor = None
+	nearest_distance = float("inf")
+	#Search through all words in the other document
+	for neighbor_index in range(0,document.shape[1]): #words are in columns so iterate over columns
+		neighbor_wordVec = document[:,neighbor_index]
+		neighbor_dist = distance(word, neighbor_wordVec)
+		#Update nearest neighbor and distance if a nearer neighbor is found
+		if neighbor_dist < nearest_distance:
+			nearest_neighbor = neighbor_index
+			nearest_distance = neighbor_dist
+	return (nearest_neighbor, nearest_distance) 
+
+#Solution to WMD with 1 constraint (see Kusner et. al, 2015)
+#For each word in document 1, find its nearest neighbor in document 2
+#Transport all its weight (as given by its normalized bag-of-words entry) to that nearest neighbor
+def lessConstrained_docDistance(doc1, doc2, doc1_nbow):
+	distance = 0
+	for word_index in range(0,doc1.shape[1]): #transpose so that words are in rows and we can iterate over rows
+		word = doc1[:,word_index]
+		nearest_word, nearest_distance = find_wordNN(word, doc2)
+		distance += nearest_distance #* doc1_nbow[word_index]
+	return distance
+
+#Perform nearest neighbor search in both directions
+#(find nearest neighbors in document 2 of words in document 1 and vice versa)
+#take the maximum for a pretty tight bound on exact WMD, but faster (Kusner et. al, 2015)
+def rWMD(doc1, doc2, doc1_nbow, doc2_nbow):
+	return max(lessConstrained_docDistance(doc1, doc2, doc1_nbow), lessConstrained_docDistance(doc2, doc1, doc2_nbow))
+
+#Given a query document and a training set, return k nearest neighbors
+def kNN(query_doc, query_nbow, train_data, train_nbow, num_neighbors):
+	#Keep track of nearest neighbors and their distances (initially we have none)
+	nn_distances = [float("inf")] * num_neighbors #keep ordered list of k nearest neighbors, largest to smallest distance
+	nearest_neighbors = [None] * num_neighbors #keep track of indices of nearest neighbors
+
+	#Documents are entries in a list of training data (each document is a matrix whose columns are word vectors)
+	for train_doc_index in xrange(0,train_data.size):
+		train_doc = train_data[train_doc_index]
+		dist = rWMD(query_doc, train_doc, query_nbow, train_nbow)
+		nn_count = 0 #we'll be iterating over the list of nearest neighbors we have so far
+		while dist < nn_distances[nn_count]: #see if this distance is shorter than the distance to a nearest neighbor
+			nn_count += 1
+			if nn_count >= num_neighbors: #we've gone through all the nearest neighbors
+				break
+		if nn_count > 0: #this is one of the k nearest neighbors so far
+			nn_distances.insert(nn_count - 1, dist)
+			nearest_neighbors.insert(nn_count, train_doc_index) #keep nn list in sync with nn distance list
+			nearest_neighbors.pop(0) #remove the furthest-away NN (as it's no longer among the closest known k)
+	return nearest_neighbors
+
+#include labels if you want to test performance
+def testWithLabels(num_neighbors, train_data, test_data, train_nbow, test_nbow, train_labels, test_labels):
+	correct_preds = 0
+	num_preds = 0
+	num_test = test_data.size
+	for query_index in xrange(0,num_test):
+		query_doc = test_data[query_index]
+		query_nbow = test_nbow[query_index]
+		query_NN = kNN(query_doc,query_nbow,train_data,train_nbow,num_neighbors)
+		NN_labels = train_labels[query_NN]
+		predicted_label = mode(NN_labels)[0][0] #get modal value
+		if predicted_label == test_labels[query_index]:
+			correct_preds += 1
+		num_preds += 1
+		print("Made %d of %d predictions" % (num_preds, num_test))
+	accuracy = float(correct_preds) / test_labels.size
+	return accuracy
+
+
+if __name__ == "__main__":
+	#User arguments: train_data_file test_data_file num_neighbors  <optional train labels> <optional test labels>
+	#If user provides train and test labels then they want to compute accuracy on a set of documents
+	#If user doesn't provide train and test labels then they want to get the nearest neighbors of a single query
+	train_data_file = sys.argv[1]
+	test_data_file = sys.argv[2]
+	num_neighbors = int(sys.argv[3])
+
+	#Load in train data now
+	with open(train_data_file,"r") as tdf:
+		[train_data, train_nbow, CTr, wordsTr] = pickle.load(tdf)
+	print train_nbow[3].shape, wordsTr[3].shape		
+	if len(sys.argv) == 6: #user has provided labels
+		train_labels_file = sys.argv[4]
+		test_labels_file = sys.argv[5]
+		#Load in train and test labels
+		with open(train_labels_file,"r") as trlf:
+			train_labels = pickle.load(trlf)
+		with open(test_labels_file, "r") as telf:
+			test_labels = pickle.load(telf)	
+		#since we have labels, we know that test data is a collection of documents
+		with open(test_data_file, "r") as tedf:
+			[test_data, test_nbow, CTe, wordsTe] = pickle.load(tedf)
+		test_accuracy = testWithLabels(num_neighbors, train_data, test_data, train_nbow, test_nbow, train_labels, test_labels)   
+		print "Test accuracy: ", test_accuracy
+
+	else: #user hasn't provided labels
+		#so now we know test data is a single document
+		with open(test_data_file, "r") as tedf:
+			[test_data, test_nbow, CTe, wordsTe] = pickle.load(tedf)
+		recommendations = kNN(test_data, test_nbow, train_data, train_nbow, num_neighbors)
+		print "We recommend the following documents: ", recommendations
+
+
---
+++ process_data.py
@@ -0,0 +1,107 @@
+import gensim, pdb, sys, scipy.io as io, numpy as np, pickle, string
+
+# read datasets line by line
+def read_line_by_line(dataset_name,C,model,vec_size):
+	# get stop words (except for twitter!)
+	SW = set()
+	for line in open('stop_words.txt'):
+		line = line.strip()
+		if line != '':
+			SW.add(line)
+
+	stop = list(SW)
+
+
+	f = open(dataset_name)
+	if len(C) == 0:
+		C = np.array([], dtype=np.object)
+	num_lines = sum(1 for line in open(dataset_name))
+	y = np.zeros((num_lines,))
+	X = np.zeros((num_lines,), dtype=np.object)
+	BOW_X = np.zeros((num_lines,), dtype=np.object)
+	count = 0
+	remain = np.zeros((num_lines,), dtype=np.object)
+	the_words = np.zeros((num_lines,), dtype=np.object)
+	for line in f:
+		print '%d out of %d' % (count+1, num_lines)
+		line = line.strip()
+		line = line.translate(string.maketrans("",""), string.punctuation)
+		T = line.split('\t')
+		classID = T[0]
+		if classID in C:
+			IXC = np.where(C==classID)
+			y[count] = IXC[0]+1
+		else:
+			C = np.append(C,classID)
+			y[count] = len(C)
+		W = line.split()
+		F = np.zeros((vec_size,len(W)-1))
+		#F = np.zeros((len(W)-1, vec_size))
+		
+		inner = 0
+		RC = np.zeros((len(W)-1,), dtype=np.object)
+		word_order = np.zeros((len(W)-1), dtype=np.object)
+		bow_x = np.zeros((len(W)-1,))
+		for word in W[1:len(W)]:
+			try:
+				test = model[word]
+				if word in stop:
+					word_order[inner] = ''
+					continue
+				if word in word_order:
+					IXW = np.where(word_order==word)
+					bow_x[IXW] += 1
+					word_order[inner] = ''
+				else:
+					word_order[inner] = word
+					bow_x[inner] += 1
+					F[:,inner] = model[word]
+					#F[inner,:] = model[word]
+			except KeyError, e:
+				#print 'Key error: "%s"' % str(e)
+				word_order[inner] = ''
+			inner = inner + 1
+		Fs = F.T[~np.all(F.T == 0, axis=1)]
+		#Fs = F[~np.all(F == 0, axis=1)]
+		word_orders = word_order[word_order != '']
+		bow_xs = bow_x[bow_x != 0]
+		X[count] = Fs.T
+		the_words[count] = word_orders
+		BOW_X[count] = bow_xs
+		count = count + 1;
+	return (X,BOW_X,y,C,the_words)
+
+def save_data(dataset,save_file_data,save_file_labels,model,vec_size):
+	# 2. read document data
+	(X,BOW_X,y,C,words)  = read_line_by_line(dataset,[],model,vec_size)
+
+	# 3. save pickle of extracted variables
+	with open(save_file_data, 'w') as f:
+		pickle.dump([X, BOW_X, C, words], f)
+
+	with open(save_file_labels, 'w') as labels_file:
+		pickle.dump(y,labels_file)
+	
+def main():
+	# 0. load word2vec model (trained on Google News)
+	#model = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+	model = gensim.models.Word2Vec.load("GoogleNews_vectors")
+	vec_size = 300
+
+	# 1. specify train/test datasets
+	train_dataset = sys.argv[1] # e.g.: 'twitter.txt'
+	test_dataset = sys.argv[2] #e.g. twitter_test.txt'
+	train_name = train_dataset.split(".")[0]
+	test_name = test_dataset.split(".")[0]
+	
+	train_data_file = train_name + ".pk"
+	test_data_file = test_name + ".pk"
+	train_labels_file = train_name + "_labels.pk"
+	test_labels_file = test_name + "_labels.pk"
+
+	# 2. read and save document data
+	save_data(train_dataset,train_data_file,train_labels_file,model,vec_size)
+	save_data(test_dataset,test_data_file,test_labels_file,model,vec_size)
+	
+if __name__ == "__main__":
+	main()						                                                                     
